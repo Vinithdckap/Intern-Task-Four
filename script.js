39a716097ba4adc8b181f02a90a2a40780f10795
Vinith Krishnan <vinithkdckap@gmail.com>
2023-11-09 16:03:59 +0530

    responsive completed fully except functions

--- script.js
+++ script.js
@@ -1,63 +1,16 @@
 
 
 let hoverView = document.querySelector(".hoverView");
-let hoverSection = document.querySelector(".hover-service-section")
-let hoverWork = document.querySelector(".hoverWork")
-let hoverWorkSection =document.querySelector(".hover-work-section")
-
-
-
-hoverWork.addEventListener("mouseover",()=>{
-    hoverWorkSection.classList.add("workShow");
-})
-
-hoverWork.addEventListener("mouseout",()=>{
-    hoverWorkSection.classList.remove("workShow")
-})
-
-
-hoverView.addEventListener("click",()=>{
-    hoverWorkSection.style.display="block"
-})
-
-
-
+let hoverSection = document.querySelector(".hover-service-section");
 
 
 hoverView.addEventListener("mouseover",()=>{
-
-    hoverSection.style.display="block";
-    
+   hoverSection.style.display ="block"
 })
 
 
-hoverView.addEventListener("mouseout",()=>{
-
-    hoverSection.style.display="none";
-    
-})
-
- let menuIcon = document.querySelector(".menuIcon");
- let cancelIcon = document.querySelector(".cancelIcon");
- let topSide = document.querySelector(".topSide-header");
- let clickBackground = document.querySelector(".click");
 
 
- menuIcon.addEventListener("click",()=>{
 
-    cancelIcon.style.display="block";
-    menuIcon.style.display="none";
-    topSide.style.display="block";
-    clickBackground.style.display="block"
-    
-
- })
-
- cancelIcon.addEventListener("click",()=>{
-    menuIcon.style.display="block";
-    cancelIcon.style.display="none";
-    topSide.style.display="none";
-
- })
 
  
