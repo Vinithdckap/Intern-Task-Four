fe239b3732890369b3f5da45bc805edf55c72b71
Vinith Krishnan <vinithkdckap@gmail.com>
2023-11-15 14:28:02 +0530

    toggle completed

--- script.js
+++ script.js
@@ -2,15 +2,81 @@
 
 let hoverView = document.querySelector(".hoverView");
 let hoverSection = document.querySelector(".hover-service-section");
+let secondNav = document.querySelector(".second-content-container");
+let innerContainer = document.querySelector(".inner-content");
+
+let hoverWork = document.querySelector(".hoverWork");
+let hoverWorkSection = document.querySelector(".hover-work-section")
 
 
-hoverView.addEventListener("mouseover",()=>{
-   hoverSection.style.display ="block"
+
+// console.log(innerContainer);
+
+
+
+
+hoverView.addEventListener("mouseover", () => {
+   hoverSection.style.display = "block";
+   // innerContainer.style.opacity = "20%"
+
+
+})
+hoverView.addEventListener("mouseout", () => {
+   hoverSection.style.display = "none"
+   // innerContainer.style.opacity = "100%"
+
+})
+hoverSection.addEventListener("mouseover", () => {
+   hoverSection.style.display = "block";
+})
+hoverSection.addEventListener("mouseout", () => {
+   hoverSection.style.display = "none";
+})
+
+
+hoverWork.addEventListener("mouseover",()=>{
+   hoverWorkSection.style.display = "block";
+});
+
+hoverWork.addEventListener("mouseout",()=>{
+   hoverWorkSection.style.display = "none";
+})
+
+
+hoverWorkSection.addEventListener("mouseover",()=>{
+   hoverWorkSection.style.display = "block"
+});
+
+hoverWorkSection.addEventListener("mouseout",()=>{
+   hoverWorkSection.style.display = "none"
+});
+
+
+
+
+// mobile menu toggle functions
+
+
+let menuIcon = document.querySelector(".menuIcon");
+let cancelIcon = document.querySelector(".cancelIcon");
+let headerMobile = document.querySelector(".headerContents");
+let subMenu = document.querySelector(".subMenu-mobile");
+
+menuIcon.addEventListener("click",()=>{
+
+   cancelIcon.style.display="block";
+   menuIcon.style.display="none";
+   headerMobile.style.display="block";
+
+})
+
+cancelIcon.addEventListener("click",()=>{
+   menuIcon.style.display="block";
+   cancelIcon.style.display="none"
+   headerMobile.style.display="none"; 
+   
 })
 
 
 
 
-
-
- 
